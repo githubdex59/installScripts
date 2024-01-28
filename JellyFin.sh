0a4ce16298f83b43ddde96d67fad406a984f13fc
githubdex59 <90550567+githubdex59@users.noreply.github.com>
2024-01-28 08:53:12 +0000

    Create JellyFin.sh

---
+++ JellyFin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "This should be ran as root"
+add-apt-repository universe
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.jellyfin.org/jellyfin_team.gpg.key | gpg --dearmor -o /etc/apt/keyrings/jellyfin.gpg
+export VERSION_OS="$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )"
+export VERSION_CODENAME="$( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )"
+export DPKG_ARCHITECTURE="$( dpkg --print-architecture )"
+cat <<EOF | tee /etc/apt/sources.list.d/jellyfin.sources
+Types: deb
+URIs: https://repo.jellyfin.org/${VERSION_OS}
+Suites: ${VERSION_CODENAME}
+Components: main
+Architectures: ${DPKG_ARCHITECTURE}
+Signed-By: /etc/apt/keyrings/jellyfin.gpg
+EOF
+apt update
+apt install jellyfin-server
